2299cb6ffc0f071c22b759406f956d9d59652ed9
Julian Rawcliffe <julianrawcliffe@gmail.com>
2017-03-17 07:51:32 +0000

    Added model validators

--- app/models/commission_type.rb
+++ app/models/commission_type.rb
@@ -1,2 +1,9 @@
 class CommissionType < ApplicationRecord
+  belongs_to :request
+  validates :name, :presence => true
+  validates :days_to_complete, numericality: { greater_than: 1 }
+
+  def name_how_long
+    "#{name} - #{days_to_complete} days"
+  end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,25 @@
+#class EmailValidator < ActiveModel::EachValidator
+#  def validate_each(record, attribute, value)
+#    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+#      record.errors[attribute] << (options[:message] || "is not an email")
+#    end
+#  end
+#end
 class Request < ApplicationRecord
+  has_one :commission_type
+  validates :commission_type_id, :presence => true
+  validates_associated :commission_type
+  validates :fullname, :presence => true
+  validates :due_date, :presence => true
+  validate :verify_contact_address, :on => :create
+  
+  # Define a custom validator for the contact_address
+  def verify_contact_address
+    unless contact_address =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      errors.add(:contact_address,  "is not an email address")
+    end
+  end
+  # Make sure a valid commission type has been selected
+  def check_commission_type
+  end
 end
