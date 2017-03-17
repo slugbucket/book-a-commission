2e3fe287930734e75176f72670ce779af4d9ab4a
slugbucket <julianrawcliffe@gmail.com>
2017-03-17 21:33:01 +0000

    Added basic functionality for a datepicker for the due date

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20170315205600) do
     t.string   "fullname"
     t.string   "contact_address",    limit: 64,    null: false
     t.integer  "commission_type_id"
-    t.datetime "due_date",                         null: false
+    t.date     "due_date",                         null: false
     t.text     "comments",           limit: 65535
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
